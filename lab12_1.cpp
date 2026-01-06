51ca0d7c374ca8a75e1e58a7dac6091d25b417a4
kennumonenumone-cell <kennumonenumone@gmail.com>
2026-01-06 15:40:26 +0700

    commit add lab12_1.cpp

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int n);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+
+int fibonacci(int n){
+    if(n == 0) return 0;
+    if(n == 1) return 1;
+
+    int a = 0, b = 1, c;
+    for(int i = 2; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
